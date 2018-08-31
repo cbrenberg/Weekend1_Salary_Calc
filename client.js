59bfb81b61124de4a10dc0803d0f6355d000f1e6
Christopher Brenberg <cbrenberg@gmail.com>
2018-08-31 16:27:08 -0500

    base mode functionality complete, also updates monthly total upon deletion

--- client.js
+++ client.js
@@ -2,12 +2,19 @@ console.log('client.js');
 
 $(document).ready(onReady)
 
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+});
+
 function onReady() {
   console.log('jQuery')
   //click listener for submit button, runs addEmployee
   $('#submitButton').on('click', addEmployee)
   //click listener on .deleteButton class within tbody, runs deleteEmployee
   $('#employeeList').on('click', '.deleteButton', deleteEmployee)
+  updateTotalMonthly();
 }//end onReady
 
 //add new employee from input values and clears inputs
@@ -26,24 +33,36 @@ function addEmployee() {
         <td>${lastName}</td>
         <td>${employeeId}</td>
         <td>${title}</td>
-        <td>${salary}</td>
+        <td class="employeeSalary">${salary}</td>
         <td><button class="deleteButton">Remove</button></td>
       </tr>`
     );//end append row
     $('input').val('');
   }//end if statement
+  updateTotalMonthly();
 }// end addEmployee
 
 //deletes selected row, re-runs updateTotalMonthly
 function deleteEmployee() {
   $(this).closest('tr').remove();
-
+  updateTotalMonthly();
 }
 
 //updates value of total monthly salary by looping through all salaries
+//refreshes totalMonthlyDiv with updated value of totalMonthly
 function updateTotalMonthly() {
   let totalMonthly = 0;
-  //for loop on nth-child of each tr?
-
+  //select all td of class .employeeSalary
+  //add each value to totalMonthly counter variable
+  $('.employeeSalary').each(function() {
+    totalMonthly += Number($(this).text());    
+  });//end .each loop
+  console.log(totalMonthly);
+  //display total monthly value on DOM
+  $('#totalMonthlyDiv').html(`<h2>Total Monthly: ${formatter.format(totalMonthly)}</h2>`);
+  //check if total > 20000. if yes, change #totalMonthlyDiv background color to red
+  if(totalMonthly > 20000) {
+    $('#totalMonthlyDiv').css('background-color', 'red');
+  }//end if statement
 }//end updateTotalMonthly
 
